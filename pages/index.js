f5c33e7da85c9f20bac8f955b7d939175f5e5ea7
cpaulintra <73374894+cpaulintra@users.noreply.github.com>
2020-10-27 05:02:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,16 @@ export default function Home() {
       <div className={styles.wrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>
-            Intra <a href="https://nextjs.org">Link</a>
+            Intra <a href="/index.js">Home</a>
           </h1>
-          <p><a href="https://nextjs.org">Link 1</a></p>
-          <p><a href="https://nextjs.org">Link 2</a></p>
+          <p><a href="https://nextjs.org">About Us</a></p>
+          <p><a href="https://nextjs.org">Investors</a></p>
         </header>
       </div>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Test <a href="https://nextjs.org">Next.js!</a>
+          Text <a href="https://nextjs.org">Link</a>
         </h1>
 
         <p className={styles.description}>
@@ -33,12 +33,12 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>About Us &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+            <h3>Investors &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -46,7 +46,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
+            <h3>Link 3 &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -54,7 +54,7 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Link 4 &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
